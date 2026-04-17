d5984e9a3226c90ca9b99b4999f0e095d620ae2c
hajdam <hajdam@users.sf.net>
2026-04-17 23:52:50 +0200

    State messaging rework - 7

--- modules/exbin-editor-wave/src/main/java/org/exbin/jaguif/editor/wave/settings/WaveColorSettingsApplier.java
+++ modules/exbin-editor-wave/src/main/java/org/exbin/jaguif/editor/wave/settings/WaveColorSettingsApplier.java
@@ -18,7 +18,7 @@ package org.exbin.jaguif.editor.wave.settings;
 import java.awt.Color;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import org.exbin.jaguif.action.api.ContextComponent;
+import org.exbin.jaguif.context.api.ContextComponent;
 import org.exbin.jaguif.context.api.ActiveContextProvider;
 import org.exbin.jaguif.options.settings.api.SettingsApplier;
 import org.exbin.jaguif.options.settings.api.SettingsOptionsProvider;
